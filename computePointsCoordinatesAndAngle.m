320c35226fa1422bf71b6b7979d1820deb441fa2
Danxue Huang <36866155+dh0072@users.noreply.github.com>
2018-06-07 12:20:02 -0500

    Add files via upload

---
+++ computePointsCoordinatesAndAngle.m
@@ -0,0 +1,22 @@
+function [points, angle] = computePointsCoordinatesAndAngle(origin, startPoint, d, r)
+%Given a circle locates at origin with radius r, return a list of points on
+%the circle that starts from startPoint, and the distance between each point
+%is d
+
+alpha = atan2(startPoint(2) - origin(2), startPoint(1) - origin(1));
+beta = 2 * asin(d/2/r);
+numPoints = ceil(2*pi/beta);
+
+points = [startPoint; zeros(numPoints - 1, 2)];
+angle = zeros(numPoints, 1);
+
+for i = 2:numPoints
+    theta = alpha + (i - 1) * beta;
+    points(i, 1) = origin(1) + r * cos(theta);
+    points(i, 2) = origin(2) + r * sin(theta);
+    angle(i - 1) = atan2(points(i, 2) - points(i - 1, 2), points(i, 1) - points(i - 1, 1));
+end
+% the angle for last point is computed against first point
+angle(i) = atan2(startPoint(2) - points(i, 2), startPoint(1) - points(i, 1));
+
+end
